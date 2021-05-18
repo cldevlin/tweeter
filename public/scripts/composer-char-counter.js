7c69b0ae7b74d0633bf284789940713b771388bd
Claire Devlin <claire.devlin@hotmail.com>
2021-05-18 19:18:20 -0400

    everything so far

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  const tweetBox = $('.tweet-box');
+  // let characterCount = 140;
+  tweetBox.on('input', function () {
+    // characterCount--;
+    // console.log(characterCount);
+    // console.log($(this).val().length);
+    const counter = $('.counter');
+    const output = $(this).parent().children().children('.counter')
+    console.log(output);
+    console.log(counter.val());
+    console.log($(this).val().length);
+    counter.text(140 - $(this).val().length);
+    if (counter.val() < 0) {
+      // $('.counter').addClass('negative');
+      counter.css("color", "red");
+    } else {
+      counter.css("color", "black");
+    }
+  });
+
+  console.log("yay");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
