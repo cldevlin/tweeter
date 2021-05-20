e08c317a269791abd69fc66cbc3f25ac4d176db5
Claire Devlin <claire.devlin@hotmail.com>
2021-05-20 14:02:35 -0400

    cleaned up code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,117 +4,73 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
+const createTweetElement = function (tweetObj) {
+  const $tweet = $(`<article class="tweet">
+    <header>
+      <div class="name">
+        <img src="${tweetObj.user.avatars}" alt="">
+        <h3>${tweetObj.user.name}</h3>
+      </div>
+      <h4>${tweetObj.user.handle}</h4>
+    </header>
+    <p>${escape(tweetObj.content.text)}</p>
+    <footer>
+      <div class="time"><span class="need_to_be_rendered" datetime="${tweetObj.created_at}">${tweetObj.created_at}</span></div>
+      <div class="reactions">
+        <a href=""><i class="fas fa-flag"></i></a>
+        <a href=""><i class="fas fa-retweet"></i></a>
+        <a href=""><i class="fas fa-heart"></i></a>
+      </div>
+    </footer>
+  </article>`);
+
+  return $tweet;
+};
+
+const renderTweets = function (tweetArr) {
+  for (const tweet of tweetArr) {
+    $(`#tweets-container`).prepend(createTweetElement(tweet));
+  }
+};
+
+const loadTweets = function () {
+  $.get("/tweets", function (tweetArr) {
+    renderTweets(tweetArr);
+  });
+};
 
 $(document).ready(function () {
+  loadTweets();
+
   $('#post-tweet').submit(function (event) {
-    // alert("Handler for .submit() called.");
     event.preventDefault();
+    if ($('.error-messages *').is(":visible")) {
+      $('.error-messages *').hide();
+    }
 
-    // const tweet = $('#post-tweet')
-    console.log("this----", $(this).serialize());
+    if (!$('#tweet-text').val()) {
+      $('#empty-tweet').slideDown("fast");
+      return;
+    } else if ($('#tweet-text').val().length > 140) {
+      $('#too-long').slideDown("fast");
+      return;
+    }
+
     const data = $(this).serialize();
-    console.log("data:", data);
 
-    // $.post("/tweets", data, (data) => {
-    //   console.log("line 19 data:", data);
-    //   renderTweets(data);
-    // });
     $.ajax({
       method: "POST",
       url: "/tweets",
-      data: data,
+      data
     }).then(function () {
-      console.log("data2:", data);
-      // renderTweets(data);
+      $('#tweet-text').val("");
+      loadTweets(data);
     });
-
   });
-
-
-
-  const createTweetElement = function (tweetObj) {
-    console.log("");
-    const $tweet = $(`<article class="tweet">
-  <header>
-    <div class="name">
-      <img src="${tweetObj.user.avatars}" alt="">
-      <h3>${tweetObj.user.name}</h3>
-    </div>
-    <h4>${tweetObj.user.handle}</h4>
-  </header>
-  <p>${tweetObj.content.text}</p>
-  <footer>
-    <div class="time"><span class="need_to_be_rendered" datetime="${tweetObj.created_at}">${tweetObj.created_at}</span></div>
-    <div class="reactions">
-      <a href=""><i class="fas fa-flag"></i></a>
-      <a href=""><i class="fas fa-retweet"></i></a>
-      <a href=""><i class="fas fa-heart"></i></a>
-    </div>
-  </footer>
-</article>`);
-
-    return $tweet;
-  };
-
-
-  const renderTweets = function (tweetArr) {
-    for (const each of tweetArr) {
-      $(`#tweets-container`).prepend(createTweetElement(each));
-    }
-  };
-
-  const loadTweets = function () {
-    $.get("/tweets", function (tweetArr) {
-      renderTweets(tweetArr);
-    });
-  };
-
-  loadTweets()
-
-  //temporary data
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
-  // $(function () {
-  //   const $button = $('#load-more-posts');
-  //   $button.on('click', function () {
-  //     console.log('Button clicked, performing ajax call...');
-  //     $.ajax('more-posts.html', { method: 'GET' })
-  //       .then(function (morePostsHtml) {
-  //         console.log('Success: ', morePostsHtml);
-  //         $button.replaceWith(morePostsHtml);
-  //       });
-  //   });
-  // });
-
-  renderTweets(data);
-
-
-  //event.target[0].value
-  //event.target.search.value
-
-})
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,38 +1,14 @@
 $(document).ready(function () {
   const tweetBox = $('.tweet-box');
-  // let characterCount = 140;
-  tweetBox.on('input', function () {
-    // characterCount--;
-    // console.log(characterCount);
-    // console.log($(this).val().length);
+
+  tweetBox.keyup(function () {
+
     const counter = $('.counter');
-    const output = $(this).parent().children().children('.counter')
-    // console.log(output);
-    // console.log(counter.val());
-    // console.log($(this).val().length);
+
     counter.text(140 - $(this).val().length);
     if (counter.val() < 0) {
-      // $('.counter').addClass('negative');
       counter.css("color", "red");
-    } else {
-      counter.css("color", "black");
     }
+    counter.css("color", "black");
   });
-
-  // console.log("yay");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
